342da8f1bb24cce19bbe4bc7eff57cb4e0f0f8ae
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-05 21:11:02 +0100

    Plotted a longer timeframe which contains a full year's of weather data

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,9 +4,8 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path("weather_data/sitka_weather_07-2021_simple.csv")
+path = Path("weather_data/sitka_weather_2021_simple.csv")
 lines = path.read_text().splitlines()
-
 reader = csv.reader(lines)
 header_row = next(reader)
 
@@ -25,7 +24,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color="red")
 
 # Format plot
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
