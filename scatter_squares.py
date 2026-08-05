444a0fbfc33d8230720579b486c201bea855deae
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-05 16:22:27 +0100

    Calculated data automatically

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Value", fontsize=14)
 
-# Set size of tick labels.
-ax.tick_params(labelsize=14)
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1_100_000])
 
 plt.show()
