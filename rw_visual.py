5919e7045ac837710ad35fbb7a9b3c0626531df6
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-05 17:45:41 +0100

    Styled the walk, colored the points

--- rw_visual.py
+++ rw_visual.py
@@ -11,6 +11,15 @@ while True:
     # Plot the points in the walk.
     plt.style.use("classic")
     fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values,
+        rw.y_values,
+        c=point_numbers,
+        cmap=plt.cm.Blues,
+        edgecolors="none",
+        s=15,
+    )
     ax.scatter(rw.x_values, rw.y_values, s=15)
     ax.set_aspect("equal")
     plt.show()
