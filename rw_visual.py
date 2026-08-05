415e0908b0da5dfce25602916f3ec3bcca6b2fd9
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-05 17:54:39 +0100

    Increased plot points to 50000

--- rw_visual.py
+++ rw_visual.py
@@ -5,7 +5,7 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active:
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
@@ -18,7 +18,7 @@ while True:
         c=point_numbers,
         cmap=plt.cm.Blues,
         edgecolors="none",
-        s=15,
+        s=1,
     )
     ax.scatter(rw.x_values, rw.y_values, s=15)
     ax.set_aspect("equal")
