791b89c0aec2bfa182142fef4de64b17fd0f33ac
Tiago Mendes <tiagomendes.2acc@gmail.com>
2026-08-05 20:58:48 +0100

    Imported the datetime module and and plotted the dates as well

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import csv
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 
 path = Path("weather_data/sitka_weather_07-2021_simple.csv")
@@ -8,20 +10,24 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Extract high temperatures.
-highs = []
+# Extract dates and high temperatures.
+dates, highs = [], []
+
 for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 # Plot the high temperatures.
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.plot(highs, color="red")
+ax.plot(dates, highs, color="red")
 
 # Format plot
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
